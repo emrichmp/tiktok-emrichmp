8ed3915915907d23056ae3487b6c81cb53a6fd28
emrich.perrier <emrich.perrier@tangoanalytics.com>
2022-12-06 02:23:54 -0800

    created Navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import tiktoklogo from './images/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            {/* <img className="logo" src={tiktoklogo} alt="TikTok" /> */}
+            <h1> tiktok </h1>
+        </div>
+    )
+}
+
+export default Navbar
