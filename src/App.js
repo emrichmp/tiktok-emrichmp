244d29547166ba80dc3c453fab519dd8050309f8
emrich.perrier <emrich.perrier@tangoanalytics.com>
2022-12-06 00:21:09 -0800

    changes to make 'tiktok videos' dynamic and pull/use data from the firebase db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import Video from "./Video"
 import './App.css';
+import { useEffect, useState } from "react";
+import db from './firebase.js'
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) => 
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, []);
+
+  console.log(videos)
+
   return (
     <div className="app">
       <h1>TikTok</h1>
       <div className="app_videos">
-        <Video 
-        url="https://joy.videvo.net/videvo_files/video/free/2021-04/large_watermarked/210329_06B_Bali_1080p_013_preview.mp4"
-        channel="emrichmp" 
-        description="Description"
-        sound="Yeah - Usher"
-        likes={123} 
-        messages={60} 
-        shares={13} 
-        />
-        <Video />
-        <Video />
-        <Video />
+        {videos.map(
+          ({ url, channel, description, sound, likes, messages, shares}) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              sound={sound}
+              likes={likes}
+              messages={messages} 
+              shares={shares} 
+            />
+        ))}
       </div>
     </div>
   );
