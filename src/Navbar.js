744c1b6f560a11010ab13dc48c39d4b85b907491
emrich.perrier <emrich.perrier@tangoanalytics.com>
2022-12-06 02:53:42 -0800

    css styling and added buttons for the rest of the nav menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,15 @@
 import tiktoklogo from './images/logo.png'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div className="Navbar">
-            {/* <img className="logo" src={tiktoklogo} alt="TikTok" /> */}
-            <h1> tiktok </h1>
+            <img className="logo" src={tiktoklogo} alt="TikTok" />
+            <input type="text"></input>
+            <input type="Button" value="+ Upload"></input>
+            <input type="Button" value ="Login"></input>
+            <MoreVertIcon />
         </div>
     )
 }
