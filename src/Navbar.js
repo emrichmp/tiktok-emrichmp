b4fb3dc705f9765f1dc34f02ec9f54cbb9b15980
emrich.perrier <emrich.perrier@tangoanalytics.com>
2022-12-06 08:13:12 -0800

    added search icon and added divs for css styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,21 @@
 import tiktoklogo from './images/logo.png'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import './Navbar.css'
 
 const Navbar = () => {
     return (
         <div className="Navbar">
             <a href="/"><img className="logo" src={tiktoklogo} alt="TikTok"/></a>
-            <input className="searchBar" type="text" placeholder="Search Accounts"></input>
-            <input className="uploadButton" type="Button" value="+ Upload"></input>
-            <input className="loginButton" type="Button" value ="Log in"></input>
-            <MoreVertIcon />
+            <div className="searchInputs">
+                <input className="searchBar" type="text" placeholder="Search Accounts"></input>
+                    <SearchRoundedIcon className="searchRoundedIcon"/>
+            </div>
+            <div className="buttons">
+                <input className="uploadButton" type="Button" value="+ Upload"></input>
+                <input className="loginButton" type="Button" value ="Log in"></input>
+                <MoreVertIcon />
+            </div>
         </div>
     )
 }
