2549b0a57b503721efd715019a3af5c36eb23604
emrich.perrier <emrich.perrier@tangoanalytics.com>
2022-12-06 02:52:19 -0800

    made changes to css tags to unwrap navbar

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+      <div className="main">
       <div className="app_videos">
         {videos.map(
           ({ url, channel, description, sound, likes, messages, shares}) => (
@@ -32,6 +33,7 @@ function App() {
             />
         ))}
       </div>
+    </div>
     </div>
   );
 }
