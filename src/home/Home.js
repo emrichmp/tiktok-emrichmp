f0e21c36d6e87d979d8c9efc3e7778769ce2f32c
Emrich-Michael Perrier <emrichmp@gmail.com>
2023-08-01 17:53:18 -0700

    App.js to Home.js Router Rework
    Had to change App.js to Home.js to accommodate React Router, switched the CSS to matched that.

---
+++ src/home/Home.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../Navbar"
+import Sidebar from "../Sidebar"
+import Video from "../Video"
+import db from '../firebase/firebase'
+import './Home.css'
+
+function Home() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        db.collection("videos").onSnapshot((snapshot) => 
+        setVideos(snapshot.docs.map((doc) => doc.data()))
+        );
+    }, []);
+
+    return (
+        <div className="Home">
+            <Navbar />
+            <div className="main">
+                <Sidebar/>
+                <div className="feed">
+                    <div className="app_videos">
+                        {videos.map(
+                            ({ url, channel, description, sound, likes, messages, shares}) => (
+                            <Video
+                            url={url}
+                            channel={channel}
+                            description={description}
+                            sound={sound}
+                            likes={likes}
+                            messages={messages} 
+                            shares={shares}
+                            />
+                            ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
