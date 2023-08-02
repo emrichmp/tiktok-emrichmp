4b60e832c1cbfc8f86eafe8a38bfee7a9497b167
Emrich-Michael Perrier <emrichmp@gmail.com>
2023-08-01 18:48:12 -0700

    Added Header

--- src/signUpPage/Header.js
+++ src/signUpPage/Header.js
@@ -1,7 +1,10 @@
+import tiktoklogo from '../images/logo.png'
+import './Header.css'
+
 const Header = () => {
     return (
-        <div className="Header">
-                HEADER
+        <div className="Navbar">
+            <a href="/"><img className="logo" src={tiktoklogo} alt="TikTok"/></a>
         </div>
     )
 }
